6b4a264c790bfbf69ee7900a5e68cc57ea4ed97d
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-12-12 14:24:18 +0900

    added amazon transcribe

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,24 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "slangbootcampfrontene0217640": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "speechGeneratora239dcbe": {
+            "region": "string",
+            "language": "string",
+            "voice": "string"
+        },
+        "transcription0e88a3c3": {
+            "region": "string",
+            "language": "string"
+        }
+    }
+}
